e8dcea631cc86ecb9adda51e4ccb749fc9d3c7d7
Dorimanx <yuri@bynet.co.il>
2015-11-30 18:28:46 +0200

    correct build script.

--- build_busybox.sh
+++ build_busybox.sh
@@ -2,7 +2,9 @@
 
 #We need full compiler including the *.o files! as they used to finish the build for static busybox.
 #If compiler is in GIT directory then all *.o files are deleted in make clen command.
-CC_STRIP=Android-ToolChain/bin/arm-eabi-strip
-$CC_STRIP --strip-unneeded busybox
-$CC_STRIP --strip-debug busybox
 make -j16
+if [ -f busybox ]; then
+	CC_STRIP=Android-ToolChain/bin/arm-eabi-strip
+	$CC_STRIP --strip-unneeded busybox
+	$CC_STRIP --strip-debug busybox
+fi;
