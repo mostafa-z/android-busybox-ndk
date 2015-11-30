37f58a178c009eb905c0279f95af9e93bab7f73b
Dorimanx <yuri@bynet.co.il>
2015-11-30 17:59:01 +0200

    build script. strip debug

--- build_busybox.sh
+++ build_busybox.sh
@@ -2,11 +2,7 @@
 
 #We need full compiler including the *.o files! as they used to finish the build for static busybox.
 #If compiler is in GIT directory then all *.o files are deleted in make clen command.
-#make -j 16 ARCH=arm CROSS_COMPILE=/android-kernel/LG/LINARO_COMPILER/bin/arm-LG-linux-gnueabi-
-#CC_STRIP=/android-kernel/LG/LINARO_COMPILER/bin/arm-LG-linux-gnueabi-strip
-#$CC_STRIP --strip-unneeded busybox
-#$CC_STRIP --strip-debug busybox
-
-# use NDK 10d to compile. see .config for path.
+CC_STRIP=Android-ToolChain/bin/arm-eabi-strip
+$CC_STRIP --strip-unneeded busybox
+$CC_STRIP --strip-debug busybox
 make -j16
-
